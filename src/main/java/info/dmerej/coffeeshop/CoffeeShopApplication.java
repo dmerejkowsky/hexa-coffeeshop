98b068747d442c7e0e7cd0c00eac297e5dd02070
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-06-18 13:05:14 +0200

    Coffeeshop -> CoffeeShop

--- src/main/java/info/dmerej/coffeeshop/CoffeeShopApplication.java
+++ src/main/java/info/dmerej/coffeeshop/CoffeeShopApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoffeeshopApplication {
+public class CoffeeShopApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CoffeeshopApplication.class, args);
+        SpringApplication.run(CoffeeShopApplication.class, args);
     }
 
 }
